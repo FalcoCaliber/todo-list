b59be87e0080202c4eb08ba1401675d16028905b
Joseph Aguilar <josephraguilar@gmail.com>
2018-06-20 16:18:11 -0700

    most functionality in loose order. code still needs cleanup and styling in desperate need.

--- js/scripts.js
+++ js/scripts.js
@@ -2,35 +2,90 @@
 // var complete =
 document.getElementById('addItemToList').addEventListener('click', function() {
   let value = document.getElementById('NewItem').value;
-  if (value)  addNewItemTodo(value);
+  if (value) {
+    addNewItemTodo(value);
+    document.getElementById('NewItem').value = "";
+  }
 })
 
+// edits text on an li entry
+
+function editText () {
+  let thisLi = this.parentNode.parentNode;
+  let newText = document.createTextNode(prompt("write a new to do!", "Walk the dog."))
+  thisLi.classList.add("thisLi");
+  thisLi.removeChild(thisLi.childNodes[0]);
+  thisLi.insertBefore(newText,thisLi.childNodes[0])
+  // thisLi.innerText = "";
+
+  // thisLi.NewItem = prompt("Write a new To-Do!", "Walk the Dog");
+
+}
+
+// moves items from incomplete to complete & Back.
+
+function completeItem() {
+  let item = this.parentNode.parentNode;
+  let parent = this.parentNode.parentNode.parentNode;
+  let id = parent.id;
+  let completeParent = this.parentNode;
+  let complete = this;
+
+  var target = (id === "incomplete") ? document.getElementById("complete") : document.getElementById("incomplete");
+
+  parent.removeChild(item);
+  target.insertBefore(item, target.childNodes[0]);
+  completeParent.removeChild(complete);
 
 
-// adds item to the to do list
+  let redo = document.createElement('button');
+  redo.classList.add("buttons");
+  redo.innerText = (id === "complete") ? "Complete" : "Re-Do";
+
+  completeParent.appendChild(redo);
+  redo.addEventListener("click", completeItem);
+
+}
+
+// removes item form the list
+function removeItem() {
+  let parent = this.parentNode.parentNode.parentNode;
+  let item = this.parentNode.parentNode
+  parent.removeChild(item);
+}
+
+
+// adds item to the to do list from text entry
 function addNewItemTodo(NewItem) {
   var doneList = document.getElementById("complete")
   var notDoneList = document.getElementById("incomplete")
 
-var item = document.createElement("li");
-item.innerText = NewItem;
+  let item = document.createElement("li");
+  item.innerText = NewItem;
 
-var buttons = document.createElement("div");
-buttons.classList.add('buttons');
+  let buttons = document.createElement("div");
+  buttons.classList.add('buttons');
 
-var remove = document.createElement("button");
-buttons.classList.add("remove");
-remove.innerText = "Delete";
+  let remove = document.createElement("button");
+  buttons.classList.add("remove");
+  remove.innerText = "Delete";
+  remove.addEventListener("click", removeItem);
 
-var complete = document.createElement('button');
-complete.classList.add("complete");
-complete.innerText = "Complete";
+  let edit = document.createElement("button");
+  buttons.classList.add("edit");
+  edit.innerText = "Edit";
+  edit.addEventListener("click", editText)
 
-buttons.appendChild(remove);
-buttons.appendChild(complete);
-item.appendChild(buttons);
+  let complete = document.createElement('button');
+  complete.classList.add("complete");
+  complete.innerText = "Complete";
 
-notDoneList.insertBefore(item, notDoneList.childNodes[0]);
+  complete.addEventListener("click", completeItem);
 
+  buttons.appendChild(remove);
+  buttons.appendChild(complete);
+  buttons.appendChild(edit);
+  item.appendChild(buttons);
 
+  notDoneList.insertBefore(item, notDoneList.childNodes[0]);
 }
